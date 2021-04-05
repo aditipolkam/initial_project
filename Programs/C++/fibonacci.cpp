f7afe5540aed194c6c97571bcbf825236693a5b7
aditipolkam <aditipolkam@gmail.com>
2021-04-05 21:17:28 +0530

    Updated fibonacci

--- Programs/C++/fibonacci.cpp
+++ Programs/C++/fibonacci.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-void main()
+#include<iostream>
+int main()
 {
 	int sum = 0,n1 = 0,n2 = 1,n3 = 2, limit = 4000000;
 	while(n3 < limit)
@@ -10,5 +10,6 @@ void main()
 		n1 = n2;
 		n2 = n3;
 	}
-	printf("%d is the sum of even-valued terms.",sum);
+	cout<<sum<<" is the sum of even-valued terms.";
+	return 0;
 }
